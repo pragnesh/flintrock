7098fe18a9b7e4eae33312cac2e9972738b591fb
pragnesh <pg.radadia@gmail.com>
2019-12-19 12:28:33 +0530

    spark change for large shuffles

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -19,4 +19,5 @@ export SPARK_PUBLIC_DNS="$(curl --silent http://169.254.169.254/latest/meta-data
 # TODO: Set a high ulimit for large shuffles
 # Need to find a way to do this, since "sudo ulimit..." doesn't fly.
 # Probably need to edit some Linux config file.
-# ulimit -n 1000000
+ulimit -n 65536
+ulimit -u 65536
