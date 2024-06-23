993f03dcd05fee2de61336355fbaf5ff1b8ce920
Pragnesh <pg.radadia@gmail.com>
2024-06-23 14:37:34 +0530

    use java 17 for spark

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -38,3 +38,8 @@ fi
 export PYSPARK_PYTHON="python3"
 ulimit -n 65536
 ulimit -u 65536
+
+#set java 17
+export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.`uname -p`
+export PATH=$JAVA_HOME/bin:$PATH
+export JDK_JAVA_OPTIONS="-XX:+UseParallelGC -XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED -Djdk.reflect.useDirectMethodHandle=false"
