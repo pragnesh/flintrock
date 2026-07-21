1ff2ad722db9b70e5b1f87c340072d36e146b98c
pragnesh <pradadiya@adelement.com>
2026-07-21 18:17:58 +0530

    use java 17 for hadoop

--- flintrock/templates/hadoop/conf/hadoop-env.sh
+++ flintrock/templates/hadoop/conf/hadoop-env.sh
@@ -1,3 +1,7 @@
 export HADOOP_HOME="$HOME/hadoop"
 export HADOOP_SSH_OPTS="-o StrictHostKeyChecking=no -o ConnectTimeout=5"
 export HADOOP_CLASSPATH=$HADOOP_HOME/share/hadoop/tools/lib/*
+#set java 17
+export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.`uname -p`
+export PATH=$JAVA_HOME/bin:$PATH
+export JDK_JAVA_OPTIONS="-XX:+UseParallelGC -XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED -Djdk.reflect.useDirectMethodHandle=false"
