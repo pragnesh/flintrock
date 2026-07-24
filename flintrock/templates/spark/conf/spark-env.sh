91a076ab0b1240936cb916f59b190e71b1f8378b
pragnesh <pradadiya@adelement.com>
2026-07-24 11:06:04 +0530

    add hadoop native library env in spark config

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -43,3 +43,7 @@ ulimit -u 65536
 export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.`uname -p`
 export PATH=$JAVA_HOME/bin:$PATH
 export JDK_JAVA_OPTIONS="-XX:+UseParallelGC -XX:+IgnoreUnrecognizedVMOptions --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED -Djdk.reflect.useDirectMethodHandle=false"
+
+#export variable required for hadoop native libraray
+export HADOOP_HOME="$HOME/hadoop"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HADOOP_HOME/lib/native
